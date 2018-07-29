9ba055e0d444098a5a781113a716d45c5fe090b5
Nick Mistretta <nickmistretta9@gmail.com>
2018-07-29 18:19:09 -0400

    Added category model with associated controller and views

--- Tangy/Data/Migrations/20180729221201_AddedCategoryModelToDatabase.Designer.cs
+++ Tangy/Data/Migrations/20180729221201_AddedCategoryModelToDatabase.Designer.cs
@@ -11,9 +11,10 @@ using Tangy.Data;
 namespace Tangy.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180729221201_AddedCategoryModelToDatabase")]
+    partial class AddedCategoryModelToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
